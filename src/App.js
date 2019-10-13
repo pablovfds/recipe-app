6dfabd30d240691d008d3876953c38dff1cabeb5
Pablo Santos <pablo.santos@ccc.ufcg.edu.br>
2019-10-13 19:29:28 -0300

    Adding a new style to recipes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import Recipe from "./Recipe";
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Button, ButtonToolbar, Form, Col } from "react-bootstrap";
 
 const App = () => {
-
   const APP_ID = "73807f54";
   const APP_KEY = "c4417d823131ed2927b9b61784755a76";
 
   const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('chicken');
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     // console.log("Effect has been run!");
@@ -17,52 +19,57 @@ const App = () => {
   }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
     const data = await response.json();
     setRecipes(data.hits);
-  }
+  };
 
-  const updateSearch = (event) => {
+  const updateSearch = event => {
     setSearch(event.target.value);
-    console.log(search)
-  }
+    console.log(search);
+  };
 
-  const getSearch = (event) => {
+  const getSearch = event => {
     event.preventDefault();
     setQuery(search);
-    setSearch('');
-  }
+    setSearch("");
+  };
 
-  return(
+  return (
     <div className="App">
-      <form onSubmit={getSearch} className="search-form">
-        <input 
-          className="search-bar" 
-          type="text"
-          value={search}
-          onChange={updateSearch}
-          />
-        <button 
-          className="search-button" 
-          type="submit">
-            Search
-          </button>
-      </form>
-      <div className="recipes">
-        {
-          recipes.map(recipe => (
-            <Recipe 
-              key={recipe.recipe.label} 
-              label={recipe.recipe.label} 
-              calories={recipe.recipe.calories} 
-              image={recipe.recipe.image}
-              ingredients={recipe.recipe.ingredients}
+      <Form onSubmit={getSearch} className="search-form">
+        <Form.Row>
+          <Col>
+            <Form.Control
+              className="search-bar"
+              type="text"
+              value={search}
+              onChange={updateSearch}
+              placeholder="Search"
             />
-          ))
-        }
+          </Col>
+          <ButtonToolbar>
+            <Button variant="info" type="submit">
+              Search
+            </Button>
+          </ButtonToolbar>
+        </Form.Row>
+      </Form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            label={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import style from './recipe.module.css'
+import React from "react";
+import style from "./recipe.module.css";
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 
-const Recipe = ({label, calories, image, ingredients}) => {
-    return(
-        <div className={style.recipe}>
-            <h1>{label}</h1>
-            <p>{calories}</p>
-            <ol>
-                {
-                    ingredients.map((ingredient) => (
-                        <li key={ingredient.text}>{ingredient.text}</li>  
-                    ))
-                }
-            </ol>
-            <img className={style.image} src={image} alt=""></img>
-        </div>
-    );
-}
+const Recipe = ({ label, calories, image, ingredients }) => {
+  const convertCalories = actualCalories => {
+    const input = parseFloat(actualCalories);
+    if (Number.isNaN(input)) {
+      return "";
+    }
+    
+    return (input).toFixed(2);
+  };
+
+  return (
+    <Card className={style.recipe}>
+      <Card.Img variant="top" src={image} rounded/>
+      <Card.Body>
+        <Card.Title>{label}</Card.Title>
+        <Card.Text>Calories: {convertCalories(calories)} kcal</Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        {ingredients.map(ingredient => (
+          <ListGroupItem key={ingredient.text}>{ingredient.text}</ListGroupItem>
+        ))}
+      </ListGroup>
+    </Card>
+  );
+};
 
 export default Recipe;
